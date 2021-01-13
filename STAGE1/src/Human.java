238bec2cca4a641c835de070b5ca66a4ee9d72d1
LAPTOP-U4KLTSNR\alsgu <alsgur2348@naver.com>
2021-01-14 00:21:02 +0900

    수정입니다

--- STAGE1/src/Human.java
+++ STAGE1/src/Human.java
@@ -1,6 +1,6 @@
 
 public class Human {
-
+	//수정입니다.
 	//용사이름은 정해줌
 	//체력 ,마나 ,레벨
 	int Heath; //직업별로 다르게 제공--ex)전사는 체력은 높지만 딜이 약함 ,법사는 체력이 낮지만 딜이강함, 궁수는 둘 다 반반
@@ -10,12 +10,13 @@ public class Human {
 
 	public void Attack(int Damage) {
 		//입력받은 데미지만큼 상대방의 체력을 깎음
+		//몬스터의 체력을 깎아야하기 때문에 가장 중요함
+		
 	}
 	public void BasicAttack(int Job) {
 		//직업별로 데미지계산해서 punch에 대입시킴
 		int Punch;
 		Attack(Punch);
-		
-	}
+		}
 	
 }
