8242c3e56484e73cd612b1cea6734bf84937587a
Lee Sunho <sunho9889@naver.com>
2021-01-19 01:27:44 +0900

    궁수입니닷

--- STAGE1/src/Archer.java
+++ STAGE1/src/Archer.java
@@ -1,4 +1,12 @@
 
-public class Archer {
-
+public class Archer extends Human {
+	int Power;
+	public void fireArrow(int Strength){
+		Power = Strength * 250;
+		Attack(Power);
+	}
+	public void arrowRain(int Strength){
+		Power = Strength * 300;
+		Attack(Power);
+	}
 }
