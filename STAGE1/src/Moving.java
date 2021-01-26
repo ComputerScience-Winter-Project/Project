9cd4833d2d4aab1d34d219a1551c1055bc2ff787
LAPTOP-U4KLTSNR\alsgu <alsgur2348@naver.com>
2021-01-27 00:17:43 +0900

    오늘의 수정본

--- STAGE1/src/Moving.java
+++ STAGE1/src/Moving.java
@@ -8,13 +8,14 @@ public class Moving extends JFrame{
 
 	public void MyFrame() {
 		
-		setTitle("")//이 프레임은 계속해서 스테이지 1,2,3이 진행될 때까지 유지되어야함!
+		setTitle("Test");//이 프레임은 계속해서 스테이지 1,2,3이 진행될 때까지 유지되어야함!
 		setLocationRelativeTo(null);//frame을 가운데에 배치
+		setSize(1080,720);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		getContentPane().setLayout(null);
+		getContentPane().setLayout(null);//레이아웃 설정
 		//프레임에 충분히 버튼을 넣을 수 있고 이를 이용해서 전투인터페이스 생성가능
-		
-	
+		Container c = getContentPane();
+		//배경이미지와 주인공 이미지 따와서 하면 될듯
 	}
 	
 	
